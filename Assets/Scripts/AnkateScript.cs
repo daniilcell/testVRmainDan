9a6f3ca1a0e93503aa042609e790d89637810e71
daniilcell <popa.i.remeshok@gmail.com>
2024-11-09 19:32:18 +0300

    popa

--- Assets/Scripts/AnkateScript.cs
+++ Assets/Scripts/AnkateScript.cs
@@ -7,9 +7,18 @@ public class AnkateScript : MonoBehaviour
 {
     public TMP_InputField nameInputField;
     public TMP_Text nameText;
-         
+    public TMP_InputField ageInputField;
+    public TMP_Text ageText;
     public void OnButtonUserName()
     {
         nameText.text = nameInputField.text;
+    }
+
+// https://www.google.ru/
+    public void OnButtonUserAge()
+    {
+        string ageString = ageInputField.text;
+        int ageInt = int.Parse(ageString)
+
     }
 }
